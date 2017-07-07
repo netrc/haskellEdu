96fb87cbbada17c35fbbd7a4fa97051e5e503315
netrc <ric@netrc.com>
2017-07-07 19:17:17 -0400

    working; can't overload functions even with different types

--- Triangle.hs
+++ Triangle.hs
@@ -2,10 +2,15 @@
 
 
 module Triangle where
+-- where gives you a module scope
 
-{- where gives you a local scope for fun vars so you can do more work
+{- where also gives you a local scope for fun vars so you can do more work
    ... of course, still not an ordered list of instructions!!!
 -}
+
+type Length = Double
+data Triangle = Triangle { a::Length, b::Length, c::Length }
+  
 
 areaTriangleTrig  a b c = result   -- use trigonometry
     where
@@ -13,10 +18,17 @@ areaTriangleTrig  a b c = result   -- use trigonometry
     sina   = sqrt (1 - cosa ^ 2)
     height = b * sina
     result = c * height / 2
+
+-- Can't "overload" functions?!? But the types are very distinct here !!!
+
+--areaTriangleHeron :: Int -> Int -> Int -> Double
 areaTriangleHeron a b c = result           -- use Heron's formula
     where
     result = sqrt (s * (s - a) * (s - b) * (s - c))
     s      = (a + b + c) / 2
+
+areaTriangleHeron1 :: Triangle -> Double
+areaTriangleHeron1 t = 3.0
 
 
 -- Note the above doesn't work if abc are not a triangle - 3 4 9
--- ex1.hs
+++ ex1.hs
@@ -8,3 +8,6 @@ main = do
   print (areaTriangleTrig 3 4 5)
   print "world"
   print (areaTriangleHeron 3 4 5)
+  print "making triangles"
+  let t1 = Triangle 3.0 4.0 5.0
+  print "done//last statement in do block must be expression"
